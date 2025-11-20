2afdb1e982531c446571d251dacccbb78e6f4473
Yanis Kouidri <yanis.kouidri@gmail.com>
2025-11-20 18:11:17 +0000

    update: enhance SonarQube and Postgres setup with encryption and namespace creation

--- sonarqube/commands.sh
+++ sonarqube/commands.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-# Postgre database
-helm upgrade --install -n sonarqube sonar-postgres bitnami/postgresql -f postgres-values.yaml
+# Install or upgrade Postgres database
+helm upgrade --install --create-namespace -n sonarqube sonar-postgres bitnami/postgresql -f postgres-values.yaml
 
-# Sonarqube 
-helm upgrade --install -n sonarqube sonarqube sonarqube/sonarqube -f sonar-values.yaml
+# Install or upgrade Sonarqube 
+helm upgrade --install --create-namespace -n sonarqube sonarqube sonarqube/sonarqube -f sonar-values.yaml
+
+# It may be necessary to go en sonarqube.kouidri.fr/setup to upgrade database after upgrade
+
+# To encrypt:
+
+sops --encrypt --age <PUBLIC_KEY_IN_TXT_FILE> secrets.yaml > secrets.enc.yaml
 
 
-# Create a secret for Postgres password
+# To decrypt and apply
+
 export SOPS_AGE_KEY=$(cat age-key.txt)
-sops --input-type yaml --output-type yaml -d secrets.yaml.enc > secrets.yaml
-kubectl create secret generic sonarqube-postgres-secret \
-  --from-literal=password=$(yq eval '.postgresql.password' secrets.yaml) \
-  --namespace sonarqube
-rm secrets.yaml
+sops --input-type yaml --output-type yaml -d secrets.yaml.enc | kubectl apply -f -
