ea800d6d93917a158af0665e54222180ddd6a50f
Yanis Kouidri <yanis.kouidri@gmail.com>
2025-12-08 08:28:13 +0100

    fix: ssh issues

--- backups/get_backup_folder.sh
+++ backups/get_backup_folder.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 set -euo pipefail
 
+log() {
+    echo -e "[$(date '+%Y-%m-%d %H:%M:%S')] $*"
+}
+
+
 # Configuration
-VPS_NAME="ovh-vps"                                # Define in .ssh/config
-SOURCE_DIR="/home/ubuntu/backups/"    # Dossier source sur le VPS
-DEST_DIR="$HOME/vps_backups" # Dossier de destination sur ton PC
+HOME=/home/yanis
+SSH_CONFIG="$HOME/.ssh/config"                     # Chemin absolu vers le fichier SSH config
+VPS_NAME="ovh-vps"                                # Défini dans .ssh/config
+SOURCE_DIR="/home/ubuntu/backups/"               # Dossier source sur le VPS
+DEST_DIR="$HOME/vps_backups"                      # Dossier de destination sur ton PC
 
 # Copie depuis le VPS vers ton PC avec rsync
-echo "Début de la copie du backup depuis le VPS..."
-rsync -avz --delete $VPS_NAME:$SOURCE_DIR $DEST_DIR
-
-
-echo "Backup copié avec succès depuis le VPS."
+log "Début de la copie du backup depuis le VPS..."
+/usr/bin/rsync -avz --delete -e "ssh -F $SSH_CONFIG" "$VPS_NAME:$SOURCE_DIR" "$DEST_DIR"
+log "Backup copié avec succès depuis le VPS."
