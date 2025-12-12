8739e0abf87b1402bf4ffe9aabb3d9d0bb857f24
Yanis Kouidri <yanis.kouidri@ik.me>
2025-12-12 09:44:27 +0000

    refactor: improve backup scripts

---
+++ immich/postgres/README.md
@@ -0,0 +1,17 @@
+# Immich database Postgres
+
+## Backup
+
+### Crontask
+
+Do
+
+```bash
+crontab -e
+```
+
+And add this line (adapt do correct location if needed)
+
+```bash
+0 3 * * * /home/ubuntu/k3s-personal-servers/immich/postgres/backup.sh >> /home/ubuntu/backups/immich/backup-immich-db.log 2>&
+```
--- immich/postgres/backup.sh
+++ immich/postgres/backup.sh
@@ -3,6 +3,14 @@
 # Stop on errors, unset variables, and fail on pipe errors
 set -euo pipefail
 
+# Config:
+NAMESPACE="immich"
+PVC="backup-immich-pvc" # PVC to back up
+SUB_PATH=""   # Sub-path within the volume to back up
+BACKUP_PATH=/home/ubuntu/backups/immich/database/
+BACKUP_NAME=immich-postgres-$(date +%Y-%m-%d-%H%M).tar.zst
+BACKUP_FILE=$BACKUP_PATH$BACKUP_NAME
+
 # Colors for nice output
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -15,26 +23,23 @@ log() {
 }
 
 export KUBECONFIG="${KUBECONFIG:-/home/ubuntu/.kube/config}"
-NAMESPACE="immich"
+
 
 log "${YELLOW}=== Immich Library Backup Started ===${NC}"
 
-VOLUME=$(kubectl get pvc server-immich-pvc -n $NAMESPACE -ojsonpath='{.spec.volumeName}')
-log "Volume name: $VOLUME"
+VOLUME_NAME=$(kubectl get pvc $PVC -n $NAMESPACE -ojsonpath='{.spec.volumeName}')
+log "Volume name: $VOLUME_NAME"
 
-IMMICH_PATH=$(kubectl get pv $VOLUME -n $NAMESPACE -ojsonpath='{.spec.local.path}')
-log "IMMICH_PATH: $IMMICH_PATH"
-
-LIBRARY_PATH="$IMMICH_PATH/library"
-log "LIBRARY_PATH: $LIBRARY_PATH"
+VOLUME_PATH=$(kubectl get pv $VOLUME_NAME -n $NAMESPACE -ojsonpath='{.spec.local.path}')
+log "Volume path: $VOLUME_PATH"
 
 
-BACKUP_PATH=/home/ubuntu/backups/immich/server/
-BACKUP_NAME=immich-library-$(date +%Y-%m-%d-%H%M).tar.zst
-BACKUP_FILE=$BACKUP_PATH$BACKUP_NAME
+PATH_TO_BACKUP="$VOLUME_PATH/$SUB_PATH"
+log "Folder to back up: $PATH_TO_BACKUP"
+
 log "Backup file will be: $BACKUP_FILE"
 
-sudo tar --use-compress-program=zstd --acls -cpvf $BACKUP_FILE -C $LIBRARY_PATH .
+sudo tar --use-compress-program=zstd --acls -cpvf $BACKUP_FILE -C $PATH_TO_BACKUP .
 
 # Remove backups older than the 2 most recent ones
 ls -t $BACKUP_PATH*.tar.zst | tail -n +3 | xargs -I {} rm -v {} || log "No old backups to delete."
